81553b6e22f8b58cb9762f8860f339cba36334e3
feherneoh <feherneoh@gmail.com>
2013-06-19 16:38:27 +0200

    Corrected misspelling in manifest repository name

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/feherneoh/repo-manifest"}
+GITREPO=${GITREPO:-"git://github.com/feherneoh/repo-manifests"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
