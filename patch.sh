09f97b850a3f5a134c39b187696bedb9578c357a
Fehér NeoH <feherneoh@gmail.com>
2013-09-01 10:20:11 +0200

    Modified patch script to only copy files those exist in source tree

--- patch.sh
+++ patch.sh
@@ -2,7 +2,18 @@
 if [ -d sourcefix ]
 then
 	echo Patching source tree...
-	cp -Rp sourcefix/* ./
+	cd sourcefix
+	FILELIST=$(find .)
+	for FILETOPATCH in $FILELIST;
+	do
+		if [ -e ../$FILETOPATCH ]
+		then
+			echo Patching $FILETOPATCH
+			cp $FILETOPATCH ../$FILETOPATCH
+		else
+			echo $FILETOPATCH does not exist
+		fi
+	done
 else
 	echo No patches were downloaded
 fi
